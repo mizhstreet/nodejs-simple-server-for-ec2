2ac600e931324cc08c1e00beb40aa5f1235f9aa5
minh.buituan <minh.buituan@vti.com.vn>
2022-12-01 18:14:25 +0900

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+yum install git -y
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install 16.0.0
+
+npm install -g pm2 
+
+git clone https://github.com/mizhstreet/nodejs-simple-server-for-ec2.git
+
+pm2 start nodejs-simple-server-for-ec2/server/server.js
+
